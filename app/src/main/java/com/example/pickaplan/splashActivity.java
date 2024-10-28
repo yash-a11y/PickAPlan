527b9e92a246eda63dc93955ab73f1c171173627
Yash <73629345+yash-a11y@users.noreply.github.com>
2024-10-27 22:25:57 -0400

    Initial commit

---
+++ app/src/main/java/com/example/pickaplan/splashActivity.java
@@ -0,0 +1,31 @@
+package com.example.pickaplan;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class splashActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_activity);
+
+        ImageView splashImg = findViewById(R.id.splash_image);
+
+        Animation anim = AnimationUtils.loadAnimation(this,R.anim.animation);
+
+        splashImg.setVisibility(ImageView.VISIBLE);
+        splashImg.startAnimation(anim);
+
+        new Handler().postDelayed(() -> {
+            Intent intent = new Intent(splashActivity.this, MainActivity.class);
+            startActivity(intent);
+            finish();
+        }, 3000);
+    }
+}
