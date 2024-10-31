75f335e874b00bea44336202060ecfaa75979a48
virenpatel <viren000p@gmail.com>
2024-10-31 13:23:29 -0400

    signup frontend

--- app/src/main/java/com/example/pickaplan/MainActivity.java
+++ app/src/main/java/com/example/pickaplan/MainActivity.java
@@ -1,6 +1,12 @@
 package com.example.pickaplan;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,6 +21,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+
+//        Button register = findViewById(R.id.btnsignup);
+//        register.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Intent intent = new Intent(MainActivity.this, signUp.class);
+//                startActivity(intent);
+//            }
+//        });
 
     }
 }
