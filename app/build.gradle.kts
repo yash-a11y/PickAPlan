34bbad9c9c57986db72573684f9156feb1480a9c
yash <73629345+yash-a11y@users.noreply.github.com>
2024-11-01 14:38:35 -0400

    home_complete_partially

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     alias(libs.plugins.android.application)
+
 }
 
 android {
@@ -33,6 +34,11 @@ android {
 
 dependencies {
 
+    // Retrofit core
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+
+    // Converter for JSON (Gson)
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
